64dfae46034354ced44b320297c0e0329c622c33
tktb-tess <chemusiclove6174@gmail.com>
2026-06-05 20:17:08 +0900

    aha

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# base-juso
+# basejuso-playground
 
 14-bit encoding of binary data, with cursed appearance
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
-import dts from 'unplugin-dts/vite';
 
 export default defineConfig({
-  plugins: [dts()],
-  build: {
-    lib: {
-      entry: './lib/main.ts',
-      formats: ['es'],
-      fileName: 'main',
-      name: 'BaseJuso',
-    },
-  },
+  plugins: [],
+  build: {},
   preview: {
     port: 8000,
   },
