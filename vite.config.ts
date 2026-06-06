2de961b90e9a57ac38afcfbfe111065e66d41a72
tktb-tess <chemusiclove6174@gmail.com>
2026-06-06 23:06:04 +0900

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/basejuso-playground',
+  base: '/basejuso-playground/',
   build: {},
   preview: {
     port: 8000,
