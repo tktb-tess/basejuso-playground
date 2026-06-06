1be7bfd62a991d499acf32db1c4268b4561e5bfc
tktb-tess <chemusiclove6174@gmail.com>
2026-06-06 20:27:09 +0900

    same

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 // @ts-check
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/vite-plugin-svelte').SvelteConfig} */
 export default {
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess()],
   compilerOptions: {
     runes: (o) =>
       o.filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
